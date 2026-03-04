9904475bc6cf39fa087df48705c0dde07155e17a
JonzorG <Jonathan.johansson2005@gmail.com>
2026-03-04 23:30:15 +0100

    Telescope upgrade

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,10 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	branch = "master",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+	},
 	config = function()
 		local telescope = require("telescope")
 		local actions = require("telescope.actions")
@@ -15,6 +18,13 @@ return {
 			defaults = {
 				path_display = { "filename_first" },
 				layout_strategy = "horizontal",
+				file_ignore_patterns = {
+					"%.o",
+					"%.out",
+					"%.class",
+					"%.zip",
+					"%.tar",
+				},
 				layout_config = {
 					horizontal = { preview_width = 0.55 },
 					vertical = { mirror = false },
@@ -33,5 +43,8 @@ return {
 				},
 			},
 		})
+
+		-- Load the FZF extension into Telescope
+		telescope.load_extension("fzf")
 	end,
 }
